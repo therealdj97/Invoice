70c0637b062d8cbf559e58bf62a82f5e575ed883
therealdj97 <DhirajJohare@outlook.com>
2022-08-13 22:04:44 +0530

    testing

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template id="page">
+  <div class="">
+    <!-- flex flex-col mx-10 space-y-10 -->
+    <!-- <tutorial/> -->
+    <Header />
+    <sec1 />
+    <Details />
+    <footer />
+    <button type="button" class="btn btn-outline-success btn-lg hover:bg-transparent" @click="print">Print</button>
+  </div>
+</template>
+
+<script>
+import sec1Vue from "../components/sec1.vue"
+import Header from "../components/header.vue"
+import Details from "../components/Details.vue"
+import Tutorial from '../components/Tutorial.vue'
+export default {
+  components: { sec1Vue, Header, Details, Tutorial },
+  name: 'IndexPage',
+  data() {
+
+  },
+  methods: {
+    print() {
+      window.print();
+    //  this.$VueHtmlToPaper('printMe');
+      console.log("printed")
+    }
+  }
+}
+</script>
+<style>
+</style>
